9a7aac07a9a172bc0f567fff10f985fdf4e6c022
sriavr@gmail.com <sriavr@gmail.com@edc6fc54-a8a2-b902-5ab6-676187ba45ea>
2012-09-27 15:19:14 +0000

    renamed the variable names in Filesystem.c as per the C coding standards. Added a file main.c for running the vfs.

--- src/Filesystem.c
+++ src/Filesystem.c
@@ -1,111 +1,91 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "../include/FileSystem.h"
+#include "../include/Filesystem.h"
 
-typedef struct fileDescriptor
+void test_fsystem()
 {
-	char fileName[50];
-	char locationFullPath[50];
-	char fileType[10];
-	long int fileSize;
-	int locationBlockNo;
-}fDes;
+    char file_name[50];
+    int file_length,ret=0;
 
-typedef struct freeList
-{
-	int blockNo;
-	struct freeList *nextFreeBlockNo;
-}fLst;
+    printf("Enter the file name with full path: ");
+    scanf("%s",file_name);
 
-typedef struct mainHeader{
-	char fileSystemLabel[60];
-//	struct fileDescriptors fd[20];
-	long int maxNoOfFileDescriptors;
-	int fileDescriptorsUsed;
-//	struct freeList *fl;
-}mHdr;
+    printf("Enter size of file in Kilo Bytes: ");
+    scanf("%d",&file_length);
 
-typedef struct header
-{
-	fDes desc[1000000];
-	fLst list[500000];
-}head;
+    //create a virtual file system
+    ret=create_vfs(file_name,file_length);
 
-typedef struct maxBlockSize
-{
-	int blockSize;
-}mBsz;
-
-int createFsystem(char *,int );
-
-void testFsystem()
-{
-	char fileName[10];
-	int fileLength,ret=0;
-	printf("Enter the file name: ");
-	scanf("%s",fileName);
-	printf("Enter size of file in Kilo Bytes: ");
-	scanf("%d",&fileLength);
-	ret=createFsystem(fileName,fileLength);
-	if(ret==0)
-		printf("file created...");
-
+    if(ret==0)
+        printf("File system created");
 }
 
-int createFsystem(char fileName[10],int fileLength)
+int create_vfs(char file_name[50],int file_length)
 {
-	FILE *fp,*cpos;
-	mHdr *mh;
-	head *hdr;
-	int pLen,fpLen,i=0,j=0;
-	char fullpath[60],path[21]="/home/pawan/Desktop/";//giving the path to where the file will be created
+    FILE *fp,*cpos;
+    meta_header *mh;
+    header *hdr;
+    int pLen,fpLen,i=0,j=0;
+    char fullpath[60],path[21]="/home/pawan/Desktop/";//giving the path to where the file will be created
 
-	mBsz mb ;
-	mb.blockSize=1024;
-	for(i=0;path[i]!='\0';i++)
-	{
-		fullpath[i]=path[i];
-		printf(" %c%d ",fullpath[i],i);
-	}
-	//i=i+1;
-	fullpath[i]='\0';
-	printf("%d",i);
-	printf("\nfile %s \n",fullpath);
-	for(j=0;fileName[j]!='\0';i++,j++)
-	{
-		fullpath[i]=fileName[j];
-		//printf("%c  ",fullpath[i]);
-	}
-	fpLen=i+1;
-	fullpath[fpLen]='\0';
-	printf("file %s \n",fullpath);
-	//for(i=0;i<fpLen;i++)h[i]
-	//	printf("%c",fullpath[i]);
-	fp=fopen(fullpath,"w+b"); // creates the fileSystem file
-	char *memory=(char*) malloc(fileLength);
-	fwrite(memory,fileLength,1,fp); // allocates the size specified to the file
-	fclose(fp);
-	mh=(mHdr*) malloc(sizeof(mHdr));
-	for(i=0;fullpath[i]!='\0';i++)
-	{
-		mh->fileSystemLabel[i]=fullpath[i];
-		//fullpath[i]=fileName[j];
-		//printf("%c  ",fullpath[i]);
-       	}
-	mh->fileSystemLabel[i]='\0';
-	printf("file %s \n",mh->fileSystemLabel);
-	mh->fileDescriptorsUsed=0;
-	mh->maxNoOfFileDescriptors=1000000;
-	fp=fopen(fullpath,"r+b");
-	fwrite(mh,sizeof(mh),1,fp);
-	cpos=ftell(fp);
-	fclose(fp);
-	hdr=(head*) malloc(sizeof(head));
-	fp=fopen(fullpath,"r+b");
+    mBsz mb ;
+    mb.block_size=1024;
+
+    //append file name to file path
+    for(i=0; path[i]!='\0'; i++)
+    {
+        fullpath[i]=path[i];
+        printf(" %c%d ",fullpath[i],i);
+    }
+
+    fullpath[i]='\0';
+    printf("%d",i);
+    printf("\nfile %s \n",fullpath);
+
+    for(j=0; file_name[j]!='\0'; i++,j++)
+    {
+        fullpath[i]=file_name[j];
+    }
+
+    fpLen=i+1;
+    fullpath[fpLen]='\0';
+    printf("file %s \n",fullpath);
+
+    // creates the fileSystem file
+    fp=fopen(fullpath,"w+b");
+    //allocates memory for the file system
+    char *memory=(char*) malloc(file_length);
+    //save the created memory to disk
+    fwrite(memory,file_length,1,fp);
+    fclose(fp);
+
+    //allocate memory for meta header
+    mh=(meta_header*) malloc(sizeof(meta_header));
+
+    //copy filesystem label with full path to the meta header field
+    for(i=0; fullpath[i]!='\0'; i++)
+    {
+        mh->file_system_label[i]=fullpath[i];
+    }
+
+    mh->file_system_label[i]='\0';
+    printf("file %s \n",mh->file_system_label);
+    mh->file_descriptors_used=0;
+    mh->max_num_file_descriptors=1000000;
+
+    fp=fopen(fullpath,"r+b");
+    fwrite(mh,sizeof(mh),1,fp);
+
+    cpos=ftell(fp);
+    fclose(fp);
+
+    //allocate memory for header
+    hdr=(header*) malloc(sizeof(header));
+    fp=fopen(fullpath,"r+b");
 //	fp = fp + cpos;
-	fwrite(hdr,sizeof(hdr),1,fp);
-	fclose(fp);
+    fwrite(hdr,sizeof(hdr),1,fp);
+    fclose(fp);
 
-	return(0);
+    return(0);
 }
