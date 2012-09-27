8e813dac840e6ad5288a19180e01e1aa3ae0a739
sriavr@gmail.com <sriavr@gmail.com@edc6fc54-a8a2-b902-5ab6-676187ba45ea>
2012-09-27 21:05:06 +0000

    * Added a test field HEADER_TEST_FIELD to header * Corrected the create_vfs functionality * Wrote test cases for various helper functions related to create_vfs

--- trunk/src/Filesystem.c
+++ trunk/src/Filesystem.c
@@ -3,89 +3,195 @@
 #include<string.h>
 #include "../include/Filesystem.h"
 
-void test_fsystem()
+void fsystem_ui()
 {
-    char file_name[50];
+    char full_path_file_name[150];
     int file_length,ret=0;
 
-    printf("Enter the file name with full path: ");
-    scanf("%s",file_name);
+    printf("\nEnter the file name with full path: ");
+    scanf("%s",full_path_file_name);
 
-    printf("Enter size of file in Kilo Bytes: ");
+    printf("\nEnter size of file in Bytes: ");
     scanf("%d",&file_length);
 
     //create a virtual file system
-    ret=create_vfs(file_name,file_length);
+    ret=create_vfs(full_path_file_name,file_length);
 
     if(ret==0)
-        printf("File system created");
+    {
+        printf("\nSuccessfully created the file system\n");
+    }
+    else
+    {
+        printf("\nError while creating the file system\n");
+    }
+
+    //test the newly created file system
+    test_vfs(full_path_file_name);
+
+    //test case for read_meta_header() function
+    test_read_meta_header(full_path_file_name);
+
+    //test case for read_header() function
+    test_read_header(full_path_file_name);
 }
 
-int create_vfs(char file_name[50],int file_length)
+int create_vfs(char fullpath[150], int file_length)
 {
-    FILE *fp,*cpos;
+    FILE *fp;
     meta_header *mh;
     header *hdr;
-    int pLen,fpLen,i=0,j=0;
-    char fullpath[60],path[21]="/home/sridhar/Desktop/";//giving the path to where the file will be created
-
-    mBsz mb ;
-    mb.block_size=1024;
-
-    //append file name to file path
-    for(i=0; path[i]!='\0'; i++)
-    {
-        fullpath[i]=path[i];
-        printf(" %c%d ",fullpath[i],i);
-    }
-
-    fullpath[i]='\0';
-    printf("%d",i);
-    printf("\nfile %s \n",fullpath);
-
-    for(j=0; file_name[j]!='\0'; i++,j++)
-    {
-        fullpath[i]=file_name[j];
-    }
-
-    fpLen=i+1;
-    fullpath[fpLen]='\0';
-    printf("file %s \n",fullpath);
 
     // creates the fileSystem file
     fp=fopen(fullpath,"w+b");
+
     //allocates memory for the file system
     char *memory=(char*) malloc(file_length);
+
     //save the created memory to disk
-    fwrite(memory,file_length,1,fp);
+    if(fwrite(memory,file_length,1,fp) != 1)
+    {
+        printf("\nFatal error while creating VFS file system.\n");
+        return 1;
+    }
     fclose(fp);
+
+
+    fp=fopen(fullpath,"r+b");
 
     //allocate memory for meta header
     mh=(meta_header*) malloc(sizeof(meta_header));
 
     //copy filesystem label with full path to the meta header field
-    for(i=0; fullpath[i]!='\0'; i++)
-    {
-        mh->file_system_label[i]=fullpath[i];
-    }
-
-    mh->file_system_label[i]='\0';
-    printf("file %s \n",mh->file_system_label);
+    strcpy(mh->file_system_label, fullpath);
     mh->file_descriptors_used=0;
     mh->max_num_file_descriptors=1000000;
+    //write meta header to the file
+    fwrite(mh,sizeof(meta_header),1,fp);
 
-    fp=fopen(fullpath,"r+b");
-    fwrite(mh,sizeof(mh),1,fp);
-
-    cpos=ftell(fp);
-    fclose(fp);
 
     //allocate memory for header
     hdr=(header*) malloc(sizeof(header));
-    fp=fopen(fullpath,"r+b");
-//	fp = fp + cpos;
-    fwrite(hdr,sizeof(hdr),1,fp);
+    strcpy(hdr -> HEADER_TEST_FIELD, "TEST HEADER OK");
+    //write header to the files
+    fwrite(hdr,sizeof(header),1,fp);
     fclose(fp);
 
     return(0);
+}
+
+void test_vfs(char fullpath[150])
+{
+    FILE *fp;
+    meta_header *mh;
+    header *hdr;
+
+    //allocate memory for meta header
+    mh=(meta_header*) malloc(sizeof(meta_header));
+
+    fp = fopen(fullpath,"r+b");
+    //read and copy the meta header to mh
+    if(fread(mh, sizeof(meta_header), 1, fp) != 1)
+    {
+        printf("Failed to read meta header");
+        return;
+    }
+
+    print_meta_header_info(mh);
+
+    //allocate memory for header
+    hdr=(header*) malloc(sizeof(header));
+    //read and copy the header to hdr
+    if(fread(hdr, sizeof(header), 1, fp) != 1)
+    {
+        printf("\nFailed to read header");
+        return;
+    }
+
+}
+
+meta_header * read_meta_header(char fullpath[150])
+{
+    FILE *fp;
+    meta_header *mh;
+
+    //allocate memory for meta header
+    mh=(meta_header*) malloc(sizeof(meta_header));
+
+    fp = fopen(fullpath,"r+b");
+
+    //read and copy the meta header to mh
+    if(fread(mh, sizeof(meta_header), 1, fp) != 1)
+    {
+        printf("Failed to read meta header");
+        return NULL;
+    }
+
+    fclose(fp);
+    return mh;
+}
+
+header * read_header(char fullpath[150])
+{
+    FILE *fp;
+    header *hdr;
+
+    //allocate memory for header
+    hdr=(header*) malloc(sizeof(header));
+    fp = fopen(fullpath,"r+b");
+    //Set the position indicator of file pointer to the header by offsetting sizeof(meta_header) bytes
+    if(fseek(fp, sizeof(meta_header), SEEK_SET) != 0)
+    {
+        return NULL;
+    }
+
+    //read and copy the header to hdr
+    if(fread(hdr, sizeof(header), 1, fp) != 1)
+    {
+        printf("\nFailed to read header");
+        return NULL;
+    }
+
+    fclose(fp);
+    return hdr;
+}
+
+void test_read_meta_header(char fullpath[150])
+{
+    meta_header * mh =  read_meta_header(fullpath);
+    if(mh!=NULL)
+    {
+        print_meta_header_info(mh);
+    }
+    else
+    {
+        printf("\n read_meta_header() function failed");
+    }
+}
+
+void test_read_header(char fullpath[150])
+{
+    header * hdr = read_header(fullpath);
+    if(hdr!=NULL)
+    {
+        print_header_info(hdr);
+    }
+    else
+    {
+        printf("\n read_header() function failed");
+    }
+}
+
+void print_meta_header_info(meta_header * mh)
+{
+    printf("\ninformation in meta header:");
+    printf("\nfile_system_label: %s", mh -> file_system_label);
+    printf("\nmax_num_file_descriptors: %ld", mh -> max_num_file_descriptors);
+    printf("\nfile_descriptors_used: %d", mh -> file_descriptors_used);
+}
+
+void print_header_info(header * hdr)
+{
+    printf("\ninformation in header:");
+    printf("\nHEADER_TEST_FIELD: %s", hdr -> HEADER_TEST_FIELD);
 }
