f0947ab7716753436871aaa1487b0999ef8cb330
sriavr@gmail.com <sriavr@gmail.com@edc6fc54-a8a2-b902-5ab6-676187ba45ea>
2012-10-27 15:49:52 +0000

    added functionality of freelist

---
+++ src/freelist.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "../include/freelist.h"
+#include "../include/Filesystem.h"
+
+extern header *hdr;
+
+//gives the next available free block
+long int next_free_block()
+{
+    long int i=0;
+    for(i =0 ; i<MAX_NUM_OF_BLOCKS; i++)
+    {
+        if(hdr->list[i].allocated == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//to allocate 1 to the bit in freelist whose index is block_num - 1
+//used when a block is allocated
+int update_flist_allocate(long int block_num)
+{
+    if(block_num >=0 && block_num < MAX_NUM_OF_BLOCKS)
+    {
+        hdr->list[block_num].allocated = 1;
+        return 0;
+    }
+    return -1;
+}
+
+//to allocate 0 to the bit in freelist whose index is block_num - 1
+//used when a block is deallocated
+int update_flist_deallocate(long int block_num)
+{
+    if(block_num >=0 && block_num < MAX_NUM_OF_BLOCKS)
+    {
+        hdr->list[block_num].allocated = 0;
+        return 0;
+    }
+    return -1;
+}
