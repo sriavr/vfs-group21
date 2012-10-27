4d2b94c3a0cb882b5ff5ee5b99ba381da034f3d8
sriavr@gmail.com <sriavr@gmail.com@edc6fc54-a8a2-b902-5ab6-676187ba45ea>
2012-10-27 15:40:01 +0000

    Added freelist.c and freelist.h for functions related to freelist

---
+++ trunk/include/freelist.h
@@ -0,0 +1,15 @@
+#ifndef FREELIST_H_INCLUDED
+#define FREELIST_H_INCLUDED
+
+//gives the next available free block
+long int next_free_block();
+
+//to allocate 1 to the bit in freelist whose index is block_num - 1
+//used when a block is allocated
+void update_flist_allocate(long int block_num);
+
+//to allocate 0 to the bit in freelist whose index is block_num - 1
+//used when a block is deallocated
+void update_flist_deallocate(long int block_num);
+
+#endif // FREELIST_H_INCLUDED
