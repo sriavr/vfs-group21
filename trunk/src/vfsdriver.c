6f9eed77b005a1ff46a502376b8fafea55299fd7
ruchi.ipdel.it@gmail.com <ruchi.ipdel.it@gmail.com@edc6fc54-a8a2-b902-5ab6-676187ba45ea>
2012-11-03 10:15:48 +0000

    changes for stable version

--- trunk/src/vfsdriver.c
+++ trunk/src/vfsdriver.c
@@ -227,7 +227,7 @@ void exportfile ( char *P1, char *P2 )
 void searchfile ( char *P1, char *P2 )
 {
      /* Call the appropriate function with given arguments and display appropriate output on the screen */
-    search_hash(P1,P2);
+    search_file(P1,P2);
     printf("searchfile_TO_BE_DONE\n");
 }
 
