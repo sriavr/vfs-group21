f2d7c51d0e32d38cab966a8cadd0b1c41e9f5528
sriavr@gmail.com <sriavr@gmail.com@edc6fc54-a8a2-b902-5ab6-676187ba45ea>
2012-11-03 21:37:50 +0000

    changes to align nary with vfs reqmnt. modification for unmount operation and bug fixes/

--- include/dsCreator.h
+++ include/dsCreator.h
@@ -1,11 +1,15 @@
 //used by file system to create a linked list containing free list of blocks
-struct node * create_linkedlist(file_descriptor * file_descriptor_list, long int file_descriptor_list_size);
+//THIS FUNCTION MAY BE SOON DROPPED
+struct node * create_linkedlist();
 
 //used by file system to create a binary search tree with filenames with absolute path
 struct bst* create_bst(file_descriptor * file_descriptor_list, long int file_descriptor_list_size);
 
 //used by filesystem to create an empty hash table and fill it with directory info
-void fill_hashtable(struct node * hashtable[], file_descriptor * file_descriptor_list, long int file_descriptor_list_size);
+struct node* fill_hashtable(struct node * hashtable[], file_descriptor * file_descriptor_list, long int file_descriptor_list_size);
 
 //nAry Tree representing directory structure
-//nNode * create_nAry_tree(file_descriptor * file_descriptor_list, long int file_descriptor_list_size);
+nNode * create_nAry_tree(file_descriptor * file_descriptor_list, long int file_descriptor_list_size);
+
+//open the nary tree and save the nodes of nary tree into fd list
+void update_fd_list();
